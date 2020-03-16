f39fa5bc2ad1d00c47e98aa568d6090e64c25d59
Hitankar Ray <59849011+hitankar-freshworks@users.noreply.github.com>
2020-03-16 15:08:16 -0700

    added bootstrap and react-bootstrap (#4)
    * add bootstrap and react-bootstrap
    
    * added dev hub style guide as bootstrap variables
    
    * theme color fix
    
    * node modules first, then custom after

--- src/pages/Intro.js
+++ src/pages/Intro.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Container from 'react-bootstrap/Container';
 
 import { Routes } from '@/constants';
 
 const Intro = () => (
-  <div className="intro">
-    <h1 className="intro__title">Intro Page</h1>
-    <Link to={Routes.ListView}>Go to List page</Link>
-  </div>
+  <Container fluid>
+    <div className="intro">
+      <h1 className="intro__title">Intro Page</h1>
+      <Link to={Routes.ListView}>Go to List page</Link>
+    </div>
+  </Container>
 );
 
 export default Intro;

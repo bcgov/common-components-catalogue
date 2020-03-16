d7faeb066ea4dc659c5df460118d29cfd84ef0d5
Hitankar Ray <hitankar@freshworks.io>
2020-03-16 13:51:17 -0700

    node modules first, then custom after

--- src/pages/Intro.js
+++ src/pages/Intro.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
-import { Routes } from '@/constants';
 import Container from 'react-bootstrap/Container';
 
+import { Routes } from '@/constants';
 
 const Intro = () => (
   <Container fluid>

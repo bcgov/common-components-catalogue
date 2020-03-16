aa0f5094aa9a76838b7ed8c1991852d939949fa5
Hitankar Ray <hitankar@freshworks.io>
2020-03-16 13:28:49 -0700

    add bootstrap and react-bootstrap

--- src/pages/Intro.js
+++ src/pages/Intro.js
@@ -3,6 +3,8 @@ import { Link } from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
 
 import { Routes } from '@/constants';
+import Container from 'react-bootstrap/Container';
+
 
 const Intro = () => (
   <Container fluid>

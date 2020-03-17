f713719cb0ef8bf3fe06d1ae9b7e9d722d933e55
Ashton Meuser <ashtonmeuser@gmail.com>
2020-03-16 20:38:46 -0700

    Add image loader, page layout, basic header
    Intro page placeholders

---
+++ src/components/reusable/Header.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+
+import { Routes } from '@/constants';
+import Logo from '@/assets/logo.svg';
+
+const Header = () => (
+  <Container fluid className="header px-1">
+    <Row className="mx-1">
+      <Link to={Routes.Intro} className="header__logo">
+        <img src={Logo}/>
+      </Link>
+      <Link to={Routes.Intro} className="header__nav-item">Common Components Catalogue</Link>
+      <Link to={Routes.Intro} className="header__nav-item">About</Link>
+      <Link to={Routes.ListView} className="header__nav-item">Catalogue</Link>
+    </Row>
+  </Container>
+);
+
+export default Header;

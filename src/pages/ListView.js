3a0178b3ccf5dc76927a670e5b1e5d5a2d02bf58
Hitankar Ray <59849011+hitankar-freshworks@users.noreply.github.com>
2020-03-18 16:33:02 -0700

    list item loading component from json file (#13)

--- src/pages/ListView.js
+++ src/pages/ListView.js
@@ -2,13 +2,20 @@ import React from 'react';
 
 import Page from '@/layouts/Page';
 import ListItem from '@/components/ListItem';
+import Components from '@/service/data/components.json';
 
+// Storing list objects
+const ListItems = Components.map( ( component ) => (
+    <ListItem component={component} key={component.id}  />
+  )
+)
+
+// ListView Page
 const ListView = () => (
   <Page>
     <h1 className="mb-5">List of Common Components</h1>
     <div>
-      <ListItem />
-      <ListItem />
+      {ListItems}
     </div>
   </Page>
 );

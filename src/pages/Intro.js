2a40243b6ec1cab7185fecbea46002108047e27b
Ashton Meuser <ashtonmeuser@gmail.com>
2020-03-18 12:10:57 -0700

    Basic intro layout as per Roop

--- src/pages/Intro.js
+++ src/pages/Intro.js
@@ -1,13 +1,20 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
-import { Routes } from '@/constants';
 import Page from '@/layouts/Page';
+import PlaceholderImage from '@/assets/placeholder.svg';
+
+const heroStyle = {
+  backgroundImage: `url(${PlaceholderImage})`
+}
 
 const Intro = () => (
   <Page>
-    <div className="intro">
+    <Row className="intro__hero p-5 mb-5" style={heroStyle}>
       <h1 className="intro__title text-center">Welcome</h1>
+    </Row>
+    <Row>
       <h2>Purpose</h2>
       <p>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
@@ -17,24 +24,26 @@ const Intro = () => (
         sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
         est laborum.
       </p>
-      <h2>Features</h2>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-        ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-        ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-        reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
-        sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-        est laborum.
-      </p>
-      <h2>Team</h2>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-        ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-        ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-        reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
-        sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-        est laborum.
-      </p>
+    </Row>
+    <Row>
+      <Col sm>
+        <h2>Features</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+          ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+          ullamco laboris nisi ut aliquip ex ea commodo consequat.
+        </p>
+      </Col>
+      <Col sm>
+        <h2>Team</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+          ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+          ullamco laboris nisi ut aliquip ex ea commodo consequat.
+        </p>
+      </Col>
+    </Row>
+    <Row>
       <h2>Contributing</h2>
       <p>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
@@ -44,8 +53,7 @@ const Intro = () => (
         sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
         est laborum.
       </p>
-      <Link to={Routes.ListView}>Go to List page</Link>
-    </div>
+    </Row>
   </Page>
 );
 

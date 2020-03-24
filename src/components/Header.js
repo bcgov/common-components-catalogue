704125291660d3eea2065caf84109c7d9e0da366
Jared Jewitt <jared@freshworks.io>
2020-03-24 14:43:41 -0700

    Hamburger menu (#17)
    * Update Header to new designs
    
    * Refactor Header styles
    
    * Remove sign out button, update logo
    
    * Remove unused import
    
    Co-authored-by: Ashton Meuser <ashtonmeuser@gmail.com>

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,25 +1,50 @@
 import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
-import { Link } from 'react-router-dom';
+import Navbar from 'react-bootstrap/Navbar';
+import classNames from 'classnames';
+import { Link, useLocation } from 'react-router-dom';
 
 import logo from '@/assets/images/logo.svg';
 import { Routes } from '@/constants';
 
-const Header = () => (
-  <Container fluid className="header px-1">
-    <Nav>
-      <Nav.Item>
-        <Nav.Link className="header__nav-item header__logo" as={Link} to={Routes.Intro}><img src={logo} alt="Logo" /></Nav.Link>
-      </Nav.Item>
-      <Nav.Item>
-        <Nav.Link className="header__nav-item" as={Link} to={Routes.Intro}>Home</Nav.Link>
-      </Nav.Item>
-      <Nav.Item>
-        <Nav.Link className="header__nav-item" as={Link} to={Routes.ListView}>Catalogue</Nav.Link>
-      </Nav.Item>
-    </Nav>
-  </Container>
-);
+const Header = () => {
+  const { pathname } = useLocation();
+  return (
+    <Navbar className="header" collapseOnSelect expand="md" variant="dark">
+      <Container>
+        <Navbar.Brand
+          as={Link}
+          to={Routes.Intro}
+        >
+          <img
+            className="header__logo"
+            src={logo}
+            alt="Logo"
+          />
+        </Navbar.Brand>
+        <Navbar.Toggle />
+        <Navbar.Collapse>
+          <Nav className="ml-auto">
+            <Nav.Link
+              className={classNames('header__nav-item', { 'header__nav-item--selected': pathname === Routes.Intro })}
+              as={Link}
+              to={Routes.Intro}
+            >
+              Home
+            </Nav.Link>
+            <Nav.Link
+              className={classNames('header__nav-item', { 'header__nav-item--selected': pathname === Routes.ListView })}
+              as={Link}
+              to={Routes.ListView}
+            >
+              Catalogue
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
 
 export default Header;

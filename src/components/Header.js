9b070fd678b573d97154378f20fcfa878ea8c794
Ashton Meuser <ashtonmeuser@gmail.com>
2020-03-24 09:13:19 -0700

    Remove unused import

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import Button from 'react-bootstrap/Button';
 import classNames from 'classnames';
 import { Link, useLocation } from 'react-router-dom';
 
